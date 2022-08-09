f9a07d5053c2adfa0ccaf0b5c86dbae48d0ad406
kmschuler <62986406+kmschuler@users.noreply.github.com>
2022-08-09 14:12:14 +0200

    Included Contract Exercises and Added EIP-165
    Thanks to @matnad for their extensive contribution, review time and expertise shared here.

--- contracts/ISealedBidAuction.sol
+++ contracts/ISealedBidAuction.sol
@@ -4,22 +4,25 @@
 
 pragma solidity ^0.8.9;
 
+// Interface contract to sealedBidAuctionBase.sol
+
 interface ISealedBidAuction{
-	// Getter functions for public variables in target contract
-	function beneficiary() external returns (address); 
-    function biddingEnd()  external returns (uint256); 
-    function bids(address, uint256) external returns(bytes32 sealedBid, uint256 deposit);
-    function hasEnded() external returns(bool);
-    function highestBid() external returns (uint256); 
-    function highestBidder() external returns (address); 
-    function pendingReturns(address) external returns(uint256); 
-    function revealEnd() external returns (uint256); 
+    // Getter functions for public variables in target contract
+    function auctionTitle() external view returns (string memory title);
+    function beneficiary() external view returns (address beneficiary); 
+    function biddingEnd()  external view returns (uint256 timestamp); 
+    function bids(address, uint256) external view returns(bytes32 sealedBid, uint256 deposit);
+    function hasEnded() external view returns(bool hasEnded);
+    function highestBid() external view returns (uint256 bidAmount); 
+    function highestBidder() external view returns (address bidder); 
+    function pendingReturns(address bidder) external view returns(uint256 amountPending); 
+    function revealEnd() external view returns (uint256 timestamp); 
 
     // Public and external functions in the target contract
     function auctionEnd() external;
     function bid(bytes32 _sealedBid) external payable;
-    function generateSealedBid(uint256 _bidAmount, bool _isLegit, string calldata _secret) external returns(bytes32 sealedBid);
+    function generateSealedBid(uint256 _bidAmount, bool _isLegit, string calldata _secret) external pure returns(bytes32 sealedBid);
     function reveal(uint256[] calldata _bidAmounts, bool[] calldata _areLegit, string[] calldata _secrets) external; 
-    function withdraw() external returns(uint256 amount);
+    function withdraw() external returns(uint256 amountReturned);
 
 }
---
+++ contracts/sealedBidAuctionBase_IERC165.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+
+// Warning: For educational purposes only. Do not use with real funds.
+
+pragma solidity ^0.8.9;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.7/contracts/utils/introspection/IERC165.sol";
+import "./ISealedBidAuction.sol";
+
+contract SealedBidAuction is IERC165, ISealedBidAuction {
+  // Auction parameters
+  string public auctionTitle = "Sealed Bid Auction";
+  address public immutable beneficiary;
+  uint public biddingEnd;
+  uint public revealEnd;
+
+  // State of the auction
+  uint public highestBid;
+  address public highestBidder;
+  bool public hasEnded;
+
+  // Allowed withdrawals of previous bids
+  mapping(address => uint) public pendingReturns;
+
+  struct Bid {
+    bytes32 sealedBid;
+    uint deposit;
+  }
+
+  mapping(address => Bid[]) public bids;
+
+  event AuctionEnded(address winner, uint amount);
+
+  modifier onlyBefore(uint time) {
+    require(block.timestamp < time, 'too late');
+    _;
+  }
+
+  modifier onlyAfter(uint time) {
+    require(block.timestamp > time, 'too early');
+    _;
+  }
+
+  function supportsInterface(bytes4 interfaceID) external pure returns (bool) {
+    return
+        interfaceID == type(IERC165).interfaceId || // ERC165
+        interfaceID == type(ISealedBidAuction).interfaceId; // Auction Interface
+  }
+
+  constructor (address _beneficiary, uint _durationBiddingMinutes, uint _durationRevealMinutes) {
+    beneficiary = _beneficiary;
+    biddingEnd = block.timestamp + _durationBiddingMinutes * 1 minutes;
+    revealEnd = biddingEnd + _durationRevealMinutes * 1 minutes;
+  }
+
+  function bid(bytes32 _sealedBid) external payable onlyBefore(biddingEnd) {
+    Bid memory newBid = Bid({
+      sealedBid: _sealedBid,
+      deposit: msg.value
+    });
+
+    bids[msg.sender].push(newBid);
+  }
+
+  function updateBid(address _bidder, uint _bidAmount) internal returns (bool success) {
+    if (_bidAmount <= highestBid) {
+      return false;
+    }
+    if (highestBidder != address(0)) {
+      // Refund the previously highest bidder.
+      pendingReturns[highestBidder] += highestBid;
+    }
+    highestBid = _bidAmount;
+    highestBidder = _bidder;
+    return true;
+  }
+
+  function reveal(uint[] calldata _bidAmounts, bool[] calldata _areLegit, string[] calldata _secrets)
+    external
+    onlyAfter(biddingEnd)
+    onlyBefore(revealEnd)
+  {
+    uint nBids = bids[msg.sender].length;
+    require(_bidAmounts.length == nBids, 'invalid number of bid amounts');
+    require(_areLegit.length == nBids, 'invalid number of bid legitimacy indicators');
+    require(_secrets.length == nBids, 'invalid number of bid secrets');
+
+    uint totalRefund;
+    for (uint i = 0; i < nBids; i++) {
+      Bid storage bidToCheck = bids[msg.sender][i];
+      (uint bidAmount, bool isLegit, string memory secret) = (_bidAmounts[i], _areLegit[i], _secrets[i]);
+      bytes32 hashedInput = generateSealedBid(bidAmount, isLegit, secret);
+      if (bidToCheck.sealedBid != hashedInput) {
+        // Failed to reveal bid.
+        // Do not refund deposit and continue with next bid.
+        continue;
+      }
+      totalRefund += bidToCheck.deposit;
+      if (isLegit && bidToCheck.deposit >= bidAmount) {
+        bool success = updateBid(msg.sender, bidAmount);
+        if (success) {
+          totalRefund -= bidAmount;
+        }
+      }
+
+      // Prevent re-claiming the same deposit.
+      bidToCheck.sealedBid = bytes32(0);
+    }
+    if (totalRefund > 0) {
+      payable(msg.sender).transfer(totalRefund);
+    }
+  }
+
+  function withdraw() external returns (uint amount) {
+    amount = pendingReturns[msg.sender];
+    if (amount > 0) {
+      pendingReturns[msg.sender] = 0;
+      payable(msg.sender).transfer(amount);
+    }
+  }
+
+  function auctionEnd() external virtual onlyAfter(revealEnd) {
+    require(!hasEnded, 'Auction already ended');
+    emit AuctionEnded(highestBidder, highestBid);
+    hasEnded = true;
+    payable(beneficiary).transfer(highestBid);
+  }
+
+  function generateSealedBid(uint _bidAmount, bool _isLegit, string memory _secret) public pure returns (bytes32 sealedBid) {
+    sealedBid = keccak256(abi.encodePacked(_bidAmount, _isLegit, _secret));
+  }
+
+}
