10626aa6ce60530d3ecb7751725ce7dfedbd69f7
Matthias Nadler <matnad@gmail.com>
2022-11-02 07:54:39 +0100

    Update auction with fee contract
    readability and signature

--- contracts/sealedBidAuctionWithFee.sol
+++ contracts/sealedBidAuctionWithFee.sol
@@ -4,17 +4,21 @@
 
 pragma solidity ^0.8.9;
 
-import "./SealedBidAuctionBase.sol";
+import "./sealedBidAuctionBase.sol";
 
 contract AuctionWithFee is SealedBidAuction {
     address payable public feeRecipient;
 
-    constructor (address payable _feeRecipient, uint _durationBiddingMinutes, uint _durationRevealMinutes) SealedBidAuction(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, _durationBiddingMinutes, _durationRevealMinutes) {
+    constructor (
+        address payable _feeRecipient,
+        uint _durationBiddingMinutes,
+        uint _durationRevealMinutes
+    ) SealedBidAuction(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, _durationBiddingMinutes, _durationRevealMinutes) {
         auctionTitle = "Auction with a fee";
         feeRecipient = _feeRecipient;
     }
 
-    function auctionEnd() external override onlyAfter(revealEnd) {
+    function auctionEnd() public override onlyAfter(revealEnd) {
         require(!hasEnded, 'Auction already ended');
         emit AuctionEnded(highestBidder, highestBid);
         hasEnded = true;
@@ -22,5 +26,4 @@ contract AuctionWithFee is SealedBidAuction {
         feeRecipient.transfer(fee);
         payable(beneficiary).transfer(highestBid - fee);
     }
-
 }
